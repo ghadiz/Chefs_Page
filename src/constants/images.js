0d84da48803e7cc6a4fe07c6fba4209da0e97f07
ghadiz <geeziadeh@gmail.com>
2023-10-22 20:09:18 +0300

    images data

---
+++ src/constants/images.js
@@ -0,0 +1,47 @@
+import logo from "../assets/Logo.png";
+import spoon from "../assets/spoon.png";
+import corn from "../assets/food17.jfif";
+import breakfast from "../assets/food8.jfif";
+import sweets from "../assets/food9.jfif";
+import lunch from "../assets/food2.jfif";
+import ig1 from "../assets/photo_ig_1.jfif";
+import ig2 from "../assets/photo_ig_2.jpg";
+import ig3 from "../assets/photo_ig_3.jpg";
+import ig4 from "../assets/photo_ig_4.jpg";
+import ig5 from "../assets/photo_ig_5.jpg";
+import ig6 from "../assets/photo_ig_6.jpg";
+import ig7 from "../assets/photo_ig_7.jpg";
+import ig8 from "../assets/photo_ig_8.jpg";
+import ig9 from "../assets/photo_ig_9.jpg";
+import chef from "../assets/chef.png";
+import quote from "../assets/quote.png";
+import brkfst1 from "../assets/food5.png";
+import bkfst2 from "../assets/bkfst2.jpg";
+import beehive from '../assets/beehive.png';
+import frayek from '../assets/frayek.jpg';
+import croissant from '../assets/croissant.jpg'
+export default {
+  logo,
+  spoon,
+  corn,
+  breakfast,
+  sweets,
+  lunch,
+  ig1,
+  ig2,
+  ig3,
+  ig4,
+  ig5,
+  ig6,
+  ig7,
+  ig8,
+  ig9,
+  chef,
+  quote,
+  brkfst1,
+  bkfst2,
+  beehive,
+  frayek,
+  croissant
+  
+};
