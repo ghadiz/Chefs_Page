fc2c9593f72a717ab5d53da54eae5379d1eb4589
ghadiz <geeziadeh@gmail.com>
2023-10-22 20:07:45 +0300

    essentials

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Subheading from "./components/Subheading";
+import Header from "./components/Header";
+import "./App.css";
+import Categories from "./components/Categories";
+import Gallery from "./components/Gallery";
+import ImageSlider from "./components/ImageSlider";
+import Chef from "./components/Chef";
+import BreakfastList from "./components/BreakfastList";
+import { Route, Routes } from "react-router-dom";
+import "./Style.scss";
+import RecipePage from "./components/RecipePage";
+
+const App = () => {
+  return (
+    <div className="container">
+
+<Navbar/>
+      <Routes>
+        <Route path="/" element={<>
+      <Header/>
+      <Chef/>
+      <Categories/>
+      <Gallery/></>} />
+        <Route path="/breakfast" element={<BreakfastList />} />
+        <Route path="/breakfast/:id" element={<RecipePage />} />
+      </Routes>
+
+      {/* <ImageSlider/> */}
+    </div>
+  );
+};
+
+export default App;
