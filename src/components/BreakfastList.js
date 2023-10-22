f493ed1a82867fc059b0dd55dc9fe2be07da8c70
ghadiz <geeziadeh@gmail.com>
2023-10-22 20:06:45 +0300

    added breakfast list

---
+++ src/components/BreakfastList.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Subheading from "./Subheading";
+import images from "../constants/images";
+import {TiArrowBack} from  'react-icons/ti'
+import { Link } from "react-router-dom";
+import recipes from "../data/recipesData"; // Import the recipe data
+
+
+const BreakfastList = () => {
+  return (
+    <div className="breakfast__page ">
+      
+      <div className="breakfast__page-heading bfst__bg">
+      <Link to="/"><TiArrowBack  color="#dcca87" fontSize={27}/></Link>
+        <div className="breakfast__page-heading_box ">
+          <div className="app__wrapper_info">
+            
+            <Subheading title="Breakfast Lists" />
+            <h1>My Breakfasts</h1>
+            <p className="p__opensans">
+              This is the collection of my delicious breakfast recipes
+            </p>
+          </div>
+          <div className="app__wrapper_img">
+            <img src={images.brkfst1} alt="breakfast" />
+          </div>
+        </div>
+      </div>
+      <div className="breakfast-list app__bg">
+        <h2>Breakfast Recipes</h2>
+        <ul>
+          {recipes.map((item) =>(
+            <li key={item.id}>
+              <Link to={`/breakfast/${item.id}`}>
+                <h3>{item.name}</h3>
+                <p>{item.description}</p>
+              </Link>
+            </li>
+          ))}
+
+
+          {/* <li>
+            <h3>Scrambled Eggs</h3>
+            <p>A classic breakfast favorite</p>
+          </li>
+          <li>
+            <h3>Pancakes</h3>
+            <p>Fluffy and delicious pancakes</p>
+          </li>
+          <li>
+            <h3>French Toast</h3>
+            <p>Golden and sweet French toast</p>
+          </li>
+          <li>
+            <h3>French Toast</h3>
+            <p>Golden and sweet French toast</p>
+          </li>
+          <li>
+            <h3>French Toast</h3>
+            <p>Golden and sweet French toast</p>
+          </li>
+          <li>
+            <h3>French Toast</h3>
+            <p>Golden and sweet French toast</p>
+          </li> */}
+          {/* Add more breakfast items here */}
+
+
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default BreakfastList;
